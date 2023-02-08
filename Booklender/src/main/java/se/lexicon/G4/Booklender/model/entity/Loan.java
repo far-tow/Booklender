7c32213338682cc01b84366a3df1c292f8008a4c
SamuelSvahn <samuelsvahn@hotmail.com>
2023-02-08 16:32:31 +0100

    Added everything except mapping

--- Booklender/src/main/java/se/lexicon/G4/Booklender/model/entity/Loan.java
+++ Booklender/src/main/java/se/lexicon/G4/Booklender/model/entity/Loan.java
@@ -1,4 +1,111 @@
 package se.lexicon.G4.Booklender.model.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
 public class Loan {
+    @Id
+    @Column(updatable = false)
+    private Long loanId;
+
+    private LibraryUser loanTaker;
+
+    private Book book;
+
+    private LocalDate loanDate;
+    @Column(name = "concluded")
+    private boolean terminated;
+
+
+    //Constructors
+
+    public Loan() {
+    }
+
+    public Loan(Long loanId, LibraryUser loanTaker, Book book, LocalDate loanDate, boolean terminated) {
+        this.loanId = loanId;
+        this.loanTaker = loanTaker;
+        this.book = book;
+        this.loanDate = loanDate;
+        this.terminated = terminated;
+    }
+
+
+
+    // Methods
+
+    //Getters and setters
+
+    public Long getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(Long loanId) {
+        this.loanId = loanId;
+    }
+
+    public LibraryUser getLoanTaker() {
+        return loanTaker;
+    }
+
+    public void setLoanTaker(LibraryUser loanTaker) {
+        this.loanTaker = loanTaker;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public LocalDate getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(LocalDate loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public boolean isTerminated() {
+        return terminated;
+    }
+
+    public void setTerminated(boolean terminated) {
+        this.terminated = terminated;
+    }
+
+
+    //ToString and Equals/Hashcode
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Loan loan = (Loan) o;
+        return terminated == loan.terminated && Objects.equals(loanId, loan.loanId) && Objects.equals(loanTaker, loan.loanTaker) && Objects.equals(book, loan.book) && Objects.equals(loanDate, loan.loanDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(loanId, loanTaker, book, loanDate, terminated);
+    }
+
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "loanId=" + loanId +
+                ", loanTaker=" + loanTaker +
+                ", book=" + book +
+                ", loanDate=" + loanDate +
+                ", terminated=" + terminated +
+                '}';
+    }
+
 }
