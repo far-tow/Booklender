76cc1c7a2a02c96cd93d0d2706507ef45e5bdc81
SamuelSvahn <samuelsvahn@hotmail.com>
2023-02-08 15:20:50 +0100

    Initial commit

---
+++ Booklender/src/main/java/se/lexicon/G4/Booklender/BooklenderGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.G4.Booklender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklenderGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklenderGroupProjectApplication.class, args);
+	}
+
+}
---
+++ Booklender/src/test/java/se/lexicon/G4/Booklender/BooklenderGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.G4.Booklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklenderGroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
