6aa7d393ea6a749114daa2eae7d2c5560c19aeaa
Farhad Towfighian <fartow@gmail.com>
2023-02-09 15:12:09 +0100

    Done with part 2, Repositories.

--- Booklender/src/main/java/se/lexicon/G4/Booklender/repository/LibraryUserRepository.java
+++ Booklender/src/main/java/se/lexicon/G4/Booklender/repository/LibraryUserRepository.java
@@ -1,4 +1,10 @@
 package se.lexicon.G4.Booklender.repository;
 
-public interface LibraryUserRepository {
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.G4.Booklender.model.entity.LibraryUser;
+
+import java.util.Optional;
+
+public interface LibraryUserRepository extends CrudRepository<LibraryUser, Integer> {
+    Optional<LibraryUser> findByEmail(String email);
 }
--- Booklender/src/main/java/se/lexicon/G4/Booklender/repository/LoanRepository.java
+++ Booklender/src/main/java/se/lexicon/G4/Booklender/repository/LoanRepository.java
@@ -1,4 +1,13 @@
 package se.lexicon.G4.Booklender.repository;
 
-public interface LoanRepository {
+
+import org.springframework.data.repository.CrudRepository;
+import se.lexicon.G4.Booklender.model.entity.Loan;
+
+import java.util.List;
+
+public interface LoanRepository extends CrudRepository<Loan, Long> {
+    List<Loan> findByLoanTakerUserId(int userId);
+    List<Loan> findByBookBookId(int bookId);
+    List<Loan> findByTerminated(boolean terminated);
 }
