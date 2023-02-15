a93ee10ac69ab7b15f1d0cee2f8364e6af059c01
liljana ristevska <li.ristevska@gmail.com>
2023-02-15 10:46:13 +0100

    Liljanas changes

--- Booklender/src/main/java/se/lexicon/G4/Booklender/model/entity/LibraryUser.java
+++ Booklender/src/main/java/se/lexicon/G4/Booklender/model/entity/LibraryUser.java
@@ -1,13 +1,15 @@
 package se.lexicon.G4.Booklender.model.entity;
 
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.Objects;
 
-
+@Data
 @Entity
 
 public class LibraryUser {
@@ -17,13 +19,13 @@ public class LibraryUser {
     @CreationTimestamp
     private LocalDate regDate;
     @Column(nullable = false)
-    private String userName;
+    private String name;
     @Column(nullable = false, unique = true)
     private String email;
 
     public LibraryUser(LocalDate regDate, String userName, String email) {
         this.regDate = regDate;
-        this.userName = userName;
+        this.name = userName;
         this.email = email;
     }
 
@@ -32,50 +34,4 @@ public class LibraryUser {
 
     }
 
-    public int getUserId() {
-        return userId;
-    }
-
-    public LocalDate getRegDate() {
-        return regDate;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof LibraryUser)) return false;
-        LibraryUser that = (LibraryUser) o;
-        return getUserId() == that.getUserId() && Objects.equals(getRegDate(), that.getRegDate()) && Objects.equals(getUserName(), that.getUserName()) && Objects.equals(getEmail(), that.getEmail());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getUserId(), getRegDate(), getUserName(), getEmail());
-    }
-
-    @Override
-    public String toString() {
-        return "LibraryUser{" +
-                "userId=" + userId +
-                ", regDate=" + regDate +
-                ", userName='" + userName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
