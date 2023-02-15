191310c83e2ec6362616736c3740e5b0d45134b1
NiveSJ <niveathithavj@gmail.com>
2023-02-15 14:20:44 +0100

    Added Dependencies for Swagger,Spring validation and model Mapper

--- Booklender/src/main/java/se/lexicon/G4/Booklender/service/impl/BookServiceImpl.java
+++ Booklender/src/main/java/se/lexicon/G4/Booklender/service/impl/BookServiceImpl.java
@@ -1,39 +1,81 @@
 package se.lexicon.G4.Booklender.service.impl;
 
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import se.lexicon.G4.Booklender.model.dto.BookDto;
+import se.lexicon.G4.Booklender.model.entity.Book;
+import se.lexicon.G4.Booklender.repository.BookRepository;
 import se.lexicon.G4.Booklender.service.BookService;
 
-import java.util.List;
+import org.modelmapper.ModelMapper;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class BookServiceImpl implements BookService {
+    @Autowired
+    BookRepository bookRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
     @Override
     public List<BookDto> findByReserved(boolean reserved) {
-        return null;
+
+        List<Book> bookByReserved = bookRepository.findByReserved(reserved);
+        return modelMapper.map(bookByReserved, new TypeToken<List<BookDto>>() {
+        }.getType());
     }
 
     @Override
     public List<BookDto> findByAvailable(boolean available) {
-        return null;
+
+        List<Book> bookByAvailable = bookRepository.findByAvailable(available);
+        return modelMapper.map(bookByAvailable, new TypeToken<List<BookDto>>() {
+        }.getType());
     }
 
     @Override
     public List<BookDto> findByTitle(String title) {
-        return null;
+        if (title.equals(null)) throw new IllegalArgumentException("Title is Null");
+
+        List<Book> bookByTitle = bookRepository.findByTitle(title);
+        return modelMapper.map(bookByTitle, new TypeToken<List<BookDto>>() {
+        }.getType());
+
     }
 
     @Override
     public BookDto findById(int bookId) {
-        return null;
+        BookDto foundBook = null;
+
+        if (bookId == 0) throw new IllegalArgumentException("book id cannot be null");
+
+        Optional<Book> findByBook = bookRepository.findById(bookId);
+
+        if (findByBook.isPresent())
+            foundBook = modelMapper.map(findByBook.get(), BookDto.class);
+
+        return foundBook;
     }
 
     @Override
     public List<BookDto> findByAll() {
-        return null;
+        Iterable<Book> AllBook = bookRepository.findAll();
+        return modelMapper.map(AllBook, new TypeToken<List<BookDto>>() {
+        }.getType());
     }
 
     @Override
     public BookDto create(BookDto bookDto) {
+        if(bookDto == null) throw new IllegalArgumentException("book cannot be null");
+        if(bookDto.)
+
+
         return null;
+
     }
 
     @Override
