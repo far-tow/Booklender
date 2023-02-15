3057b16010bd69810035ad6e75664162b6b8aa9c
NiveSJ <niveathithavj@gmail.com>
2023-02-15 15:21:15 +0100

    Added book service class

--- Booklender/src/main/java/se/lexicon/G4/Booklender/service/impl/BookServiceImpl.java
+++ Booklender/src/main/java/se/lexicon/G4/Booklender/service/impl/BookServiceImpl.java
@@ -3,6 +3,8 @@ package se.lexicon.G4.Booklender.service.impl;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import se.lexicon.G4.Booklender.exception.DataNotFoundException;
 import se.lexicon.G4.Booklender.model.dto.BookDto;
 import se.lexicon.G4.Booklender.model.entity.Book;
 import se.lexicon.G4.Booklender.repository.BookRepository;
@@ -14,13 +16,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class BookServiceImpl implements BookService {
     @Autowired
     BookRepository bookRepository;
-
     @Autowired
     ModelMapper modelMapper;
-
     @Override
     public List<BookDto> findByReserved(boolean reserved) {
 
@@ -57,6 +58,7 @@ public class BookServiceImpl implements BookService {
 
         if (findByBook.isPresent())
             foundBook = modelMapper.map(findByBook.get(), BookDto.class);
+        else throw new DataNotFoundException("No book with the Id found");
 
         return foundBook;
     }
@@ -70,21 +72,41 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public BookDto create(BookDto bookDto) {
-        if(bookDto == null) throw new IllegalArgumentException("book cannot be null");
-        if(bookDto.)
 
-
-        return null;
+        if (bookDto == null) throw new IllegalArgumentException("book to be created cannot be null");
+        if (bookDto.getBookId() != 0) throw new IllegalArgumentException("Book with same id already created");
+        Book createdbook = bookRepository.save(modelMapper.map(bookDto, Book.class));
+        return modelMapper.map(createdbook, BookDto.class);
 
     }
 
     @Override
     public BookDto update(BookDto bookDto) {
-        return null;
+        // Find and update
+        if (bookDto == null) throw new IllegalArgumentException("Book to be updated cannot be null!!");
+
+        if (bookDto.getBookId() == 0) throw new IllegalArgumentException("Book Id cannot be null while updating");
+
+        BookDto foundBook = findById(bookDto.getBookId());
+        if (foundBook == null) throw new DataNotFoundException("No such book found to update!!");
+
+        Book createdBook = bookRepository.save(modelMapper.map(bookDto, Book.class));
+        return modelMapper.map(createdBook, BookDto.class);
     }
 
     @Override
     public boolean delete(int bookId) {
-        return false;
+
+        if (bookId == 0) throw new IllegalArgumentException("Book to be Deleted cannot be null!!");
+
+        BookDto foundBook = findById(bookId);
+        if (foundBook == null) throw new DataNotFoundException("No such book found to delete!!");
+
+        bookRepository.deleteById(bookId);
+
+        BookDto book = findById(bookId);
+        if (book != null)
+            return false;
+        return true;
     }
 }
