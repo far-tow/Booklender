178f39e385859ef06bdaa3a8ec116fbe6af931c8
Farhad Towfighian <fartow@gmail.com>
2023-02-15 13:07:01 +0100

    Dto files done. Validation annotation will be added later on.

--- Booklender/src/main/java/se/lexicon/G4/Booklender/model/dto/BookDto.java
+++ Booklender/src/main/java/se/lexicon/G4/Booklender/model/dto/BookDto.java
@@ -1,4 +1,20 @@
 package se.lexicon.G4.Booklender.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookDto {
+    private int bookId;
+    private String title;
+    private boolean available;
+    private boolean reserved;
+    private int maxLoanDays;
+    private BigDecimal finePerDay;
+    private String description;
+
 }
--- Booklender/src/main/java/se/lexicon/G4/Booklender/model/dto/LoanDto.java
+++ Booklender/src/main/java/se/lexicon/G4/Booklender/model/dto/LoanDto.java
@@ -1,4 +1,20 @@
 package se.lexicon.G4.Booklender.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import se.lexicon.G4.Booklender.model.entity.Book;
+import se.lexicon.G4.Booklender.model.entity.LibraryUser;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoanDto {
+    private long loanId;
+    private LibraryUser loanTaker;
+    private Book book;
+    private LocalDate loanDate;
+    private boolean terminate;
 }
