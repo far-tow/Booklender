925abf4bbff9224eb1eafb3be1b22aae140fda06
Farhad Towfighian <fartow@gmail.com>
2023-02-16 15:21:43 +0100

    Loan Controller implementations done!

--- Booklender/src/main/java/se/lexicon/G4/Booklender/controller/LoanController.java
+++ Booklender/src/main/java/se/lexicon/G4/Booklender/controller/LoanController.java
@@ -2,19 +2,19 @@ package se.lexicon.G4.Booklender.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import se.lexicon.G4.Booklender.model.dto.BookDto;
 import se.lexicon.G4.Booklender.model.dto.LoanDto;
 import se.lexicon.G4.Booklender.service.LoanService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -69,5 +69,38 @@ public class LoanController {
     public ResponseEntity<List<LoanDto>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(loanService.findAll());
     }
+    @Operation(summary = "To Borrow a Book")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "201", description = "Loan Founded",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(name = "Example",
+                            implementation = LoanDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request body", content = {@Content})
+    })
+    @PostMapping("/")
+    public ResponseEntity<LoanDto> create(@RequestBody @Valid LoanDto loanDto){
+        LoanDto createdLoanDto = loanService.create(loanDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdLoanDto);
+    }
+    @Operation(summary = "To Rebook or update a Loan")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Loan is updated", content = {@Content}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = {@Content})
+    })
+    @PutMapping("/")
+    public ResponseEntity<Void> update(@RequestBody @Valid LoanDto loanDto){
+        loanService.update(loanDto);
+        return ResponseEntity.noContent().build();
+    }
+    @Operation(summary = "To Delete a Loan or Borrowed Book")
+    @ApiResponses( value = {
+            @ApiResponse(responseCode = "200", description = "Loan is deleted", content = {@Content}),
+            @ApiResponse(responseCode = "400", description = "Invalid Loan id supplied", content = {@Content})
+    })
+    @DeleteMapping ("/")
+    public ResponseEntity<Void> deleteById(@RequestBody @Valid Long id){
+        loanService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
